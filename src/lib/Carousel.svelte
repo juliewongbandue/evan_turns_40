7b5b8ae650ad52ba1413495d81f7f6d69f7218cf
Julie Wongbandue <juliewongbandue@gmail.com>
2025-04-01 14:11:51 -0400

    add carousel

---
+++ src/lib/Carousel.svelte
@@ -0,0 +1,66 @@
+<script>
+  let currentIndex = 0;
+
+	let images = Array.from({length: 28}).map((image, i) => `/assets/${i + 1}.jpeg`)
+
+  function goToNext() {
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+
+  function goToPrevious() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+  }
+
+</script>
+
+<style>
+  .carousel {
+    position: relative;
+    width: 100%;
+    max-width: 600px;
+    margin: auto;
+    overflow: hidden;
+  }
+
+  .carousel-images {
+    display: flex;
+    transition: transform 0.5s ease-in-out;
+  }
+
+  .carousel-images img {
+    width: 100%;
+    max-width: 100%;
+    display: block;
+  }
+
+  .button {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+    border: none;
+    padding: 10px;
+    cursor: pointer;
+    font-size: 18px;
+  }
+
+  .prev {
+    left: 10px;
+  }
+
+  .next {
+    right: 10px;
+  }
+</style>
+
+<div class="carousel">
+  <div class="carousel-images" style="transform: translateX(-{currentIndex * 100}%)">
+    {#each images as image}
+      <img src={image} alt="carousel image" />
+    {/each}
+  </div>
+
+  <button class="button prev" on:click={goToPrevious}>&lt;</button>
+  <button class="button next" on:click={goToNext}>&gt;</button>
+</div>
--- src/routes/memories/+page.svelte
+++ src/routes/memories/+page.svelte
@@ -1,16 +1,16 @@
-
-<h1>Memories</h1>
-
-<div>
-  <img src="/assets/1.jpeg" alt="Julie and Dad" />
-</div>
-<p class="body article-small">If you have any more memories you'd like to share with us, we would love to know them, and what he meant to you. Please email any stories, and/or photos to us at: <a class='email' href="mailto:thinkingofbue@gmail.com?subject=Thinking of Bue">thinkingofbue@gmail.com.</a></p>
+<script>
+import Carousel from '../../lib/Carousel.svelte';
+</script>
 
 <style>
-.email {
-  font-family: var(--font-family-mono);
-  font-weight: 600;
-  color: var(--primary);
-  text-decoration: none;
-}
+  .email {
+    font-family: var(--font-family-mono);
+    font-weight: 600;
+    color: var(--primary);
+    text-decoration: none;
+  }
 </style>
+
+<h1>Memories</h1>
+<Carousel/>
+<p class="body article-small">If you have any more memories you'd like to share with us, we would love to know them, and what he meant to you. Please email any stories, and/or photos to us at: <a class='email' href="mailto:thinkingofbue@gmail.com?subject=Thinking of Bue">thinkingofbue@gmail.com.</a></p>
