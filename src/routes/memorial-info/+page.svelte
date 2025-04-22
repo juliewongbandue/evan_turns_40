c4d18b56961df0f0b397f2e4bb6687ee564ffd76
Julie Wongbandue <juliewongbandue@gmail.com>
2025-04-22 10:23:15 -0400

    update event info

--- src/routes/memorial-info/+page.svelte
+++ src/routes/memorial-info/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Memorial Info</h1>
 
 <section class='article-small'>
-<p class='body'>Growing up, dad would throw an annual barbeque for my birthday, which falls on or near Memorial Day Weekend. This barbeque would often turn out to be our largest party of the year. So to honor him, we will be having a memorial service in the house he made a home, in Piscataway, New Jersey.</p>
+<p class='body'>Growing up, dad would throw an annual barbeque for my birthday, which falls on or near Memorial Day Weekend. This barbeque would often turn out to be our largest party of the year. To honor him, we will be having a memorial service in the house he made a home, in Piscataway, New Jersey. Attendance to the service is optional, all are welcome to the following lunch and ongoing reception. </p>
 
 <div class='body'>
   <h2>Saturday, May 31, 2025</h2>
@@ -9,7 +9,13 @@
     <br/>
     Piscataway, NJ 08854
   </p>
-  <p class="more">More details to follow</p>
+  <h2>Schedule</h2>
+  <p class='info'>The timing below is a loose outline: </p>
+  <ul>
+    <li class="info"><b>10AM</b> - A Buddhist ceremony will be conducted in our home for close friends and family, attendance is optional. </li>
+    <li class="info"><b>1PM onwards</b> - Lunch, snacks, food, remembering Dad. Expect your usual, but slightly different, Wongbandue cookout. Come and go as you please. </li>
+  </ul>
+  <p class="more">Please email us with any questions at <a class='email' style="font-family: var(--font-family-body);" href="mailto:thinkingofbue@gmail.com?subject=Memorial Question">thinkingofbue@gmail.com</a>.</p>
 </div>
 
 </section>
@@ -25,6 +31,10 @@
 .address {
   font-size: var(--font-size-2);
 }
+
+.info {
+  font-size: var(--font-size-1);
+}
 .body > .more {
   color: var(--gray-4);
   font-size: var(--font-size-1);
@@ -32,4 +42,5 @@
   font-family: var(--font-family-body);
   margin: 0;
 }
+
 </style>
