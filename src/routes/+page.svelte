d82471487f58cae2c574b5f7706e6d8776695505
efb <evanbarden@gmail.com>
2025-07-08 23:13:04 -0400

    update proj name, main copy + images, email a ddress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,17 +43,29 @@ import RSVPButton from '../lib/rsvpButton.svelte';
   </section>
   <div class="image-wrapper" style="--image-border-color: var(--orange-1)">
     <div class='image-container' style="background-image: url('assets/evan-2.jpg'); background-size: cover; background-repeat: no-repeat; background-position: 0px -60px"></div>
-    <p class='image-description'>Some text about the photo</p>
+    <p class='image-description'>
+      It's true. I have now spent four decades slithering around on this big rock called earth.
+      I've also spent 15 glorious/delusional years in NYC.
+      To celebrate both, I'm having a big party with a heafty bar minimum for which I will need YOUR help getting through.
+      There will be music, laughter, and kind people.
+    </p>
   </div>
 
   <div class="image-wrapper" style="--image-border-color: var(--blue-9)">
-    <p class='image-description'>Some text about the photo</p>
-    <div class='image-container'></div>
+    <p class='image-description'>Like many 40-year-olds, I was once a very cute child. Illegally cute.
+      Then came self-awareness, puberty, an understanding of global injustices, and hair loss.
+      But I haven't given up just yet.
+    </p>
+    <div class='image-container' style="background-image: url('assets/evan-tball.jpg'); background-size: 170%; background-repeat: no-repeat; background-position: -140px -160px"></div>
   </div>
 
   <div class="image-wrapper">
-    <div class='image-container'></div>
-    <p class='image-description'>Some text about the photo</p>
+    <div class='image-container' style="background-image: url('assets/evan-power-joker.jpg'); background-size: 200%; background-repeat: no-repeat; background-position: -180px -60px"></div>
+    <p class='image-description'>If you're thinking, "Hmmm. Should I really care about this party?"
+      That's fair. But it doesn't look like Julie and I are having a big wedding anytime soon, so when else
+      is this gonna happen? Let's party.
+      If you don't live near NYC, I ask that you consider this your excuse to visit, or at least check out the FOMO page.
+    </p>
   </div>
 
 
@@ -114,28 +126,28 @@ import RSVPButton from '../lib/rsvpButton.svelte';
   }
 
   .text-reveal-container {
-    position: relative; 
-    width: 100vw; 
-    height: 200px; 
+    position: relative;
+    width: 100vw;
+    height: 200px;
     backdrop-filter: blur(5px) invert(10%);
     /* blue-5 */
-    background-color:rgba(0, 64, 110, 0.9); 
+    background-color:rgba(0, 64, 110, 0.9);
     -webkit-mask: url(#text-mask);
     mask: url(#text-mask);
     overflow: hidden;
   }
-  
+
   .wiper-box {
     position: absolute;
     top: 86px;
-    width: 100vw; 
+    width: 100vw;
     height: 200px;
-    background-color: var(--orange-8); 
+    background-color: var(--orange-8);
     animation: wipe-animation .5s ease-out, fallConfetti 8s linear infinite;
   }
 
   @keyframes wipe-animation {
-    0% { top: -200px } 
+    0% { top: -200px }
     100% { top: 86px }
   }
 
@@ -152,7 +164,7 @@ import RSVPButton from '../lib/rsvpButton.svelte';
     }
   }
   /* Media Query for Mobile Devices */
-  @media (max-width: 767px) { 
+  @media (max-width: 767px) {
     .info {
       gap: var(--space-3);
     }
@@ -160,8 +172,8 @@ import RSVPButton from '../lib/rsvpButton.svelte';
       text-align: center;
       gap: 0;
       margin-bottom: var(--space-4);
-      grid-template-columns: 1fr; 
-      grid-template-areas: 
+      grid-template-columns: 1fr;
+      grid-template-areas:
         "one"
         "two";
     }
