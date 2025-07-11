fff1db15f2f2ebb59db001bab8d5d23cc9c7bbf5
efb <evanbarden@gmail.com>
2025-07-10 21:22:07 -0400

    add RSVP form, FOMO button, and copy updates

---
+++ src/lib/fomoButton.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let style = ''
+</script>
+
+<div class='button-layout' style={style}>
+    <a class="button" href="https://www.google.com">I HAVE FOMO</a>
+</div>
+
+<style>
+  .button {
+  display: block;
+  font-family: var(--font-family-heading);
+  font-size: 2rem;
+  padding: var(--space-3) var(--space-4);
+  width: max-content;
+  border-radius: var(--border-radius-4);
+  text-decoration: none;
+  color: var(--orange-9);
+  background-color: var(--blue-5);
+  transition: all var(--duration-0) var(--easing-function-2);
+  }
+
+  .button:hover {
+    background-color: var(--blue-10);
+    color: var(--orange-5);
+  }
+
+  .button-layout {
+    display: flex;
+    gap: var(--space-3);
+    flex-direction: column;
+  }
+</style>
--- src/routes/party/+page.svelte
+++ src/routes/party/+page.svelte
@@ -2,7 +2,7 @@
   import RSVPButton from '../../lib/rsvpButton.svelte';
 </script>
 <div class="content">
-<h1>Party Info</h1>
+<h1>The Party</h1>
 
 <section class='article-small'>
 
@@ -15,16 +15,45 @@
     Brooklyn, NY 11211
   </p>
 
-  <h2>Schedule</h2>
-  <p class='info'>The timing below is a loose outline: </p>
+  <!-- <h2>Schedule</h2> -->
+  <p class='info'>Here's the loose timing of everything: </p>
   <ul>
-    <li class="info"><b>2PM</b> - Arrive</li>
-    <li class="info"><b>2:30PM-6PM</b> - Drinks, music, dancing, talk about Evan, or how much you're actually there for Julie.</li>
+    <li class="info"><b>2PM</b> - Arrive / make final outfit tweaks</li>
+    <li class="info"><b>2:30PM-6PM</b> - Drinks, music, dancing, talk about me, or how much you're actually there for Julie.</li>
+    <li class="info"><b>6:30PM</b> - The bar opens to regular service. You don't have to leave, but I will be going to dinner. I suggest you do the same!</li>
   </ul>
-  <p class="more">Please email me with any questions at <a class='email' href="mailto:evanfortybarden@gmail.com?subject=Party Question">evanfortybarden@gmail.com</a>.</p>
+    <!-- <h2>Dress Code</h2> -->
+  <p class="info">
+    <b>Dress code:</b> I just want to see you in your New York best. Whatever that means to you. Alternatively, do whatever you want.
+  </p>
 </div>
-
 <RSVPButton/>
+<h1>The After Party</h1>
+<div class="body">
+  <h2>Later that night!</h2>
+  <p class='info'>
+    After a break for dinner, I'll be posting up somewhere in Greenpoint/Williamsburg
+    for the rest of the night. Details to come.
+  </p>
+</div>
+<h1>Early Birds</h1>
+<div class="body">
+  <h2>Friday, September 12th, 2025</h2>
+  <p class='info'>
+    If you're in town on Friday and want to start celebrating early, I will almost definitely
+    be doing improv at Magnet Theater with The Friday Night Sh*w. 9pm!
+  </p>
+</div>
+<h1>Sickos Only</h1>
+<div class="body">
+  <h2>Sunday, September 14th, 2025</h2>
+  <p class='info'>
+    If you're an absolute psycho and like punk music, I will be going to see NY legend Jeff
+    Rosenstock and the bad boys of Ontario, PUP, at Brooklyn Paramount. <a href="https://www.ticketmaster.com/event/000062750A608011?irgwc=1&clickid=UAS15K1aYxycUaxUoESJU196UksSBeW5Ly4O180&camefrom=CFC_BUYAT_1387536&impradid=1387536&REFERRAL_ID=tmfeedbuyat1387536&wt.mc_id=aff_BUYAT_1387536&utm_source=1387536-Seated&impradname=Seated&utm_medium=affiliate&ircid=4272" target="_blank">Grab a ticket</a>, scream your head off, and ring in
+    my actual birthday with me at midnight.
+  </p>
+      <p class="more">Please email me with any questions at <a class='email' href="mailto:evanfortybarden@gmail.com?subject=Party Question">evanfortybarden@gmail.com</a>.</p>
+</div>
 
 </section>
 </div>
