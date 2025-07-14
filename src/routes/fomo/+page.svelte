8b9bc94e6d27fb87d6c36c096d853b59c5a073fd
efb <evanbarden@gmail.com>
2025-07-14 19:52:53 -0400

    update fomo copy with google voice number

--- src/routes/fomo/+page.svelte
+++ src/routes/fomo/+page.svelte
@@ -10,8 +10,8 @@
   to the party, I would still love to hear from you.
 </p>
 <p class="content">
-  I'm working on an audio guestbook that will allow you to call in and talk to your heart's desire,
-  but if you're more of a writer, or very impatient, please drop me a line at <a class='email' href="mailto:evanfortybarden@gmail.com?subject=Hi Evan">evanfortybarden at gmail</a> to share a
+  If you're the type to talk it out, please call <strong>(512) 522-7336‬</strong> and leave me a voicemail. I promise I
+  won't pick up! And if you're more of a writer, please drop me a line at <a class='email' href="mailto:evanfortybarden@gmail.com?subject=Hi Evan">evanfortybarden at gmail</a> to share a
   fond memory, what you think of my whole deal, or a time I was wrong about something.
   Anything, really. I miss you and it'll be really nice to hear from you.
 </p>
@@ -19,6 +19,9 @@
   Of course, you are always encouraged to just call or text my ass, but maybe this feels bit a nicer, or
   <em>intentional</em>, and you don't need to worry about interrupting me while I yell at someone
   for parking in a bike lane.
+</p>
+<p class="content">
+  More of a card person? Do whatever you want! You have my address.
 </p>
 <a class='email' href="mailto:evanfortybarden@gmail.com?subject=Hi Evan">
   <img src="assets/evan-fomo.jpg" width="100%" title="a strange, bald man with glasses, nearly forty years old, sits at a hightop table in a comedy club, beckoning you to contact him. in white text to the left of him, it reads, 'Please, drop me a line.' he begs, like a pathetic loser." alt="a strange, bald man with glasses, nearly forty years old, sits at a hightop table in a comedy club, beckoning you to contact him. in white text to the left of him, it reads, 'Please, drop me a line.' he begs, like a pathetic loser."/>
