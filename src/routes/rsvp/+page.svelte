976917058a27404d195aa99d3919fa8a28086a2a
efb <evanbarden@gmail.com>
2025-07-15 00:08:10 -0400

    add google map embed + link, update copy, update meta

--- src/routes/rsvp/+page.svelte
+++ src/routes/rsvp/+page.svelte
@@ -8,7 +8,7 @@
   <p>Please let me know if you'll be able to make it!</p>
   <p class="subText">(<a href="https://docs.google.com/forms/d/e/1FAIpQLScOZ-hOS_x63VWyxVd3q-u_wiOIRehrHA2vCOnbckDKmjqQ5g/viewform?usp=header" target="_blank">Click here</a> if you don't see a form below.)</p>
 
-  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScOZ-hOS_x63VWyxVd3q-u_wiOIRehrHA2vCOnbckDKmjqQ5g/viewform?embedded=true" width="100%" height="1492" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScOZ-hOS_x63VWyxVd3q-u_wiOIRehrHA2vCOnbckDKmjqQ5g/viewform?embedded=true" width="100%" height="1362" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 
 <Button href="/fomo" style='padding-top: var(--space-4)'>I have fomo :(</Button>
 </article>
